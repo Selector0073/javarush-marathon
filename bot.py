5dafb8c4fceb157ae0cae3e7efebe7e686b1e176
Selector0073 <97699633+Selector0073@users.noreply.github.com>
2026-07-07 19:20:03 +0300

    feat(/): lesson 3
    add /date and /message comands

--- bot.py
+++ bot.py
@@ -41,19 +41,77 @@ async def gpt_dialog(update, context):
 async def hello(update, context):
     if dialog.mode == "gpt":
         await gpt_dialog(update, context)
+    elif dialog.mode == "date":
+        await date_dialog(update, context)
+    elif dialog.mode == "message":
+        await message_dialog(update, context)
+
+async def date_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+    await send_photo(update, context, query)
+    await send_text(update, context, "Гарний вибірю \uD83D\uDE05 Ваша задача запросити дівчину/хлопця на побачення за 5 повідомлень.")
+    promt = load_prompt(query)
+    chatgpt.set_promt(promt)
+
+async def date_dialog(update, context):
+    text = update.message.text
+    my_message = await send_text(update, context, "Відправляю повідомлення...")
+    answer = await chatgpt.add_message(text)
+    await my_message.edit_text(answer)
+
+async def message(update, context):
+    dialog.mode = "message"
+    msg = load_message("message")
+    await send_photo(update, context, "message")
+    await send_text_buttons(update, context, msg, {
+        "message_next": "Написати повідомлення",
+        "message_date": "Запростити на побачення",
+    })
+    dialog.list.clear()
+
+async def message_dialog(update, context):
+    text = update.message.text
+    dialog.list.append(text)
+
+async def message_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+    prompt = load_prompt(query)
+    user_chat_history = "\n\n".join(dialog.list)
+    my_message = await send_text(update, context, "Думаю над варіантами...")
+    answer = await chatgpt.send_question(prompt, user_chat_history)
+    await my_message.edit_text(answer)
+
+async def date(update, context):
+    dialog.mode = "date"
+    msg = load_message("date")
+    await send_photo(update, context, "date")
+    await send_text_buttons(update, context, msg, {
+        "date_grande": "Аріана гранде",
+        "date_robby": "Марго Роббі",
+        "date_zendaya": "Зендея",
+        "date_gosling": "Раян Гослінг",
+        "date_hardy": "Том Гарді"
+    })
 
 # ---
 
 
 dialog = Dialog()
 dialog.mode = None
+dialog.list = []
 
 chatgpt = ChatGptService(token=openai_api_key)
 
 app = ApplicationBuilder().token(telegram_api_key).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("gpt", gpt))
+app.add_handler(CommandHandler("date", date))
+app.add_handler(CommandHandler("message", message))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+app.add_handler(CallbackQueryHandler(date_button, pattern="^date_.*"))
+app.add_handler(CallbackQueryHandler(message_button, pattern="^message_.*"))
 
 print("Started")
 
