772b738bed22dedab24b4a2adf936cde98e0668b
Selector0073 <97699633+Selector0073@users.noreply.github.com>
2026-07-08 18:57:45 +0300

    feat(/): lesson 4
    add /profile and /opener comands
    
    BREAKING CHANGE:

--- bot.py
+++ bot.py
@@ -9,8 +9,6 @@ load_dotenv()
 telegram_api_key = os.getenv("TELEGRAM-API-KEY")
 openai_api_key = os.getenv("OPEN-AI-API-KEY")
 
-# *ПОВНИЙ КОД МОЖНА ПЕРЕГЛЯНУТИ НА GITHUB: https://github.com/Selector0073/javarush-marathon
-
 # ---
 
 async def start(update, context):
@@ -45,6 +43,10 @@ async def hello(update, context):
         await date_dialog(update, context)
     elif dialog.mode == "message":
         await message_dialog(update, context)
+    elif dialog.mode == "profile":
+        await profile_dialog(update, context)
+    elif dialog.mode == "opener":
+        await opener_dialog(update, context)
 
 async def date_button(update, context):
     query = update.callback_query.data
@@ -83,6 +85,72 @@ async def message_button(update, context):
     answer = await chatgpt.send_question(prompt, user_chat_history)
     await my_message.edit_text(answer)
 
+async def profile(update, context):
+    dialog.mode = "profile"
+    msg = load_message("profile")
+    await send_photo(update, context, "profile")
+    await send_text(update, context, msg)
+    dialog.user.clear()
+    dialog.counter = 0
+    await send_text(update, context, "Скільки вам років?")
+
+async def profile_dialog(update, context):
+    text = update.message.text
+    dialog.counter += 1
+
+    if dialog.counter == 1:
+        dialog.user["age"] = text
+        await send_text(update, context, "Ким ви працюєте?")
+    if dialog.counter == 2:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "Яке у вас хоббі?")
+    if dialog.counter == 3:
+        dialog.user["hobby"] = text
+        await send_text(update, context, "Що вам НЕ подобається в людях?")
+    if dialog.counter == 4:
+        dialog.user["annoys"] = text
+        await send_text(update, context, "Мета знайомства")
+    if dialog.counter == 5:
+        dialog.user["goals"] = text
+        prompt = load_prompt("profile")
+        user_info = dialog_user_info_to_str(dialog.user)
+        my_message = await send_text(update, context, "Думаю...") 
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+
+async def opener(update, context):
+    dialog.mode = "opener"
+    msg = load_message("opener")
+    await send_photo(update, context, "opener")
+    await send_text(update, context, msg)
+    dialog.user.clear()
+    dialog.counter = 0
+    await send_text(update, context, "Ім'я партнера?")
+
+async def opener_dialog(update, context):
+    text = update.message.text
+    dialog.counter += 1
+
+    if dialog.counter == 1:
+        dialog.user["name"] = text
+        await send_text(update, context, "Скільки років?")
+    if dialog.counter == 2:
+        dialog.user["age"] = text
+        await send_text(update, context, "Оцініть зовнішність: 1-10 балів?")
+    if dialog.counter == 3:
+        dialog.user["handsome"] = text
+        await send_text(update, context, "Ким працює?")
+    if dialog.counter == 4:
+        dialog.user["occupation"] = text
+        await send_text(update, context, "Мета знайомства?")
+    if dialog.counter == 5:
+        dialog.user["goals"] = text
+        prompt = load_prompt("opener")
+        user_info = dialog_user_info_to_str(dialog.user)
+        my_message = await send_text(update, context, "Думаю...") 
+        answer = await chatgpt.send_question(prompt, user_info)
+        await my_message.edit_text(answer)
+
 async def date(update, context):
     dialog.mode = "date"
     msg = load_message("date")
@@ -100,7 +168,9 @@ async def date(update, context):
 
 dialog = Dialog()
 dialog.mode = None
-dialog.list = []
+dialog.user = {}
+dialog.counter = ()
+
 
 chatgpt = ChatGptService(token=openai_api_key)
 
@@ -109,6 +179,8 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("gpt", gpt))
 app.add_handler(CommandHandler("date", date))
 app.add_handler(CommandHandler("message", message))
+app.add_handler(CommandHandler("profile", profile))
+app.add_handler(CommandHandler("opener", opener))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
 app.add_handler(CallbackQueryHandler(date_button, pattern="^date_.*"))
 app.add_handler(CallbackQueryHandler(message_button, pattern="^message_.*"))
